b70efc74ce616caf5708dd1716a550557b6fe7a0
orion <orion@neural-algorithms.com>
2019-01-02 18:11:04 +0200

    finished random search

--- meetup_automl_timeseries/configurations.py
+++ meetup_automl_timeseries/configurations.py
@@ -26,7 +26,7 @@ class ConfigSpace(object):
         out_config = {}
         for name, cls in self.classes.items():
             out_config[name] = {}
-            for hp in cls.get_Hyperparameters():
+            for hp in cls.get_hyperparameters():
                 out_config[name][hp.name] = hp.random_value()
 
         return out_config
@@ -38,44 +38,7 @@ class ConfigSpace(object):
             if new_x is not None:
                 additional_features.append(new_x)
 
-        print("%d new features were added" % len(additional_features))
         if len(additional_features)>0:
             new_x = numpy.concatenate(additional_features, axis=1)
             x = numpy.concatenate((x, new_x), axis=1)
         return x
-
-#**** Example ****
-class ConstantFeature():
-
-    @classmethod
-    def run(cls, x, n_lags=1, active=1):
-        if active:
-            return numpy.ones((x.shape[0], 1))*n_lags
-        else:
-            return None
-
-    @classmethod
-    def get_Hyperparameters(cls):
-        active = HyperParameter(name='active', min_value=0, max_value=1, discrete=True)
-        n_lags = HyperParameter(name='n_lags', min_value=3, max_value=6, discrete=True)
-        return [n_lags, active]
-
-config_space = ConfigSpace([ConstantFeature])
-
-if __name__ == '__main__':
-    x = numpy.zeros((3, 1))
-    print(x)
-
-    config_space = ConfigSpace([ConstantFeature])
-    config = config_space.get_random_configuration()
-    print(config)
-    x = config_space.run_configuration(x, config)
-    print(x)
-    config = config_space.get_random_configuration()
-    print(config)
-    x = config_space.run_configuration(x, config)
-    print(x)
-    config = config_space.get_random_configuration()
-    print(config)
-    x = config_space.run_configuration(x, config)
-    print(x)
